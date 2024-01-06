effab56b1959142a68bf0c13e05f572a2daa1f07
Dmitrii Baranov <me@dmbaranov.io>
2024-01-07 00:16:40 +0200

    Added endpoint to fetch single chat

--- src/main/kotlin/org/weatherbot/admin/chat/ChatController.kt
+++ src/main/kotlin/org/weatherbot/admin/chat/ChatController.kt
@@ -1,11 +1,16 @@
 package org.weatherbot.admin.chat
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import org.weatherbot.admin.chat.db.Chat
+import java.util.*
 
 @RestController
 class ChatController(val chatService: ChatService) {
     @GetMapping("/chats")
     fun getAllChats(): Iterable<Chat> = chatService.getAllChats()
+
+    @GetMapping("/chats/{chatId}")
+    fun getSingleChat(@PathVariable chatId: String): Optional<Chat> = chatService.getSingleChat(chatId)
 }
--- src/main/kotlin/org/weatherbot/admin/chat/ChatService.kt
+++ src/main/kotlin/org/weatherbot/admin/chat/ChatService.kt
@@ -3,8 +3,10 @@ package org.weatherbot.admin.chat
 import org.springframework.stereotype.Service
 import org.weatherbot.admin.chat.db.Chat
 import org.weatherbot.admin.chat.db.ChatRepository
+import java.util.*
 
 @Service
 class ChatService(private val chatRepository: ChatRepository) {
     fun getAllChats(): Iterable<Chat> = chatRepository.findAll()
+    fun getSingleChat(chatId: String): Optional<Chat> = chatRepository.findById(chatId)
 }
